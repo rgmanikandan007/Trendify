f645740dd1348ed9e49a489ce9de0426ff3dc93e
rgmanikandan07 <rgmanikandan07@gmail.com>
2025-02-05 13:04:11 +0530

    trendify

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const app = require('./app')
+const dotenv = require('dotenv')
+const connectDatabase = require('./config/db')
+
+dotenv.config({path:'config/config.env'})
+
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening to the port: ${process.env.PORT} in ${process.env.NODE_ENV} `)
+})
